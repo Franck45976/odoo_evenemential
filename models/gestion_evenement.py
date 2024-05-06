03e7348d9caed8d514dfdf470c40bf4597704821
Vianney <zaromoussa@gmail.com>
2024-05-06 16:16:37 +0000

    test

--- models/gestion_evenement.py
+++ models/gestion_evenement.py
@@ -1,11 +1,16 @@
-from odoo impory models, fields, api
+from odoo import models, fields, api
 
-
+  #Création, modification et suppression d'événements avec titre, description, date, lieu, etc.
 class GestionEvenemnt(models.Model):
 
     _name = 'gestion_evenemnt'
     _description = 'gestion des événements'
 
+    name = fields.Char(string='Titre')
+    description = fields.Char(string='Description')
+    date_debut = fields.Date(string='Date Debut')
+    date_fin = fields.Date(string='Date Fin')
+    lieu = fields.Char(string='Lieu')
 
 
 
