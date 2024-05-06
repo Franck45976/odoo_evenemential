69b831b3f80861b7ac48085e971dfb210f8e032a
Vianney <zaromoussa@gmail.com>
2024-05-06 14:56:09 +0000

    Ajout de nouveaux fichiers pour la gestion des événements

---
+++ __manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+
+   'name' : 'GEDILG',
+   'description' : """Ce projet vise à développer un module Odoo pour la 
+   gestion des événements, des inscriptions en ligne et des participants.""",
+   'version' : '1.0',
+   #'summary' = 'Gestion des événements, des inscriptions en ligne et des participants',
+   'author' : 'Vianney.F',
+   'category' : 'nouveau',
+
+   'depends' : ['base',],
+   'data' : [],
+
+
+   'installable' : True,
+   'application' : True,
+   'auto_install' : False,
+
+
+}
---
+++ models/gestion_evenement.py
@@ -0,0 +1,24 @@
+from odoo impory models, fields, api
+
+
+class GestionEvenemnt(models.Model):
+
+    _name = 'gestion_evenemnt'
+    _description = 'gestion des événements'
+
+
+
+
+class InscriptionLigne(models.Model):
+
+    _name = 'inscription_ligne'
+    _description = ''
+
+    
+
+
+
+
+
+
+
